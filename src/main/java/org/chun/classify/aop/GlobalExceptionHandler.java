b24fafc291bf266bc55aec9a7e256185fa27f934
Chun <fyc92330.tw@gmail.com>
2023-08-27 01:37:42 +0800

    ReplyMessage Handler

--- src/main/java/org/chun/classify/aop/GlobalExceptionHandler.java
+++ src/main/java/org/chun/classify/aop/GlobalExceptionHandler.java
@@ -4,17 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.chun.classify.exception.LineReplyMessageExpiredException;
 import org.chun.classify.listener.event.LineReplyMessageExpiredEvent;
-import org.springframework.context.ApplicationEventPublisher;
+import org.chun.classify.listener.exchange.LineReplyMessageExpiredExchange;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-	private final ApplicationEventPublisher eventPublisher;
+	private final LineReplyMessageExpiredExchange lineReplyMessageExpiredExchange;
 
 	@ExceptionHandler(LineReplyMessageExpiredException.class)
 	private void handleLineReplyMessageExpired(LineReplyMessageExpiredException e) {
-		eventPublisher.publishEvent(new LineReplyMessageExpiredEvent(e.getMessages(), e.getReceiptUserId()));
+		lineReplyMessageExpiredExchange.handle(new LineReplyMessageExpiredEvent(e.getMessages(), e.getReceiptUserId()));
 	}
 }
